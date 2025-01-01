83b91674c4d7d357afe1e891e86d4cd2cd14e4e7
claudinxch <claudiohenrique0909@gmail.com>
2025-01-01 19:56:14 -0300

    Finished implementing some Linked List methods

--- dsa/linked-lists.py
+++ dsa/linked-lists.py
@@ -90,6 +90,96 @@ class LinkedList:
             current_node = current_node.next
 
         return array
+    
+    def count_nodes(self) -> int:
+        if not self.head: return 0
+
+        count = 1
+        current_node = self.head
+
+        while current_node.next:
+            count += 1
+            current_node = current_node.next
+
+        return count
+    
+    def remove_at(self, index):
+        if index < 0 or index >= self.count_nodes():
+            raise Exception("Invalid Index.")
+        
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        current_node = self.head
+        while current_node:
+            if count == index - 1:
+                current_node.next = current_node.next.next
+                break
+
+            current_node = current_node.next
+            count += 1
+
+    def insert_at(self, index, data):
+        if index < 0 or index >= self.count_nodes():
+            raise Exception("Invalid Index.")
+        
+        if index == 0:
+            self.prepend(data)
+            return
+                
+        count = 0
+        current_node = self.head
+        new_node = Node(data)   
+        while current_node:
+            if count == index - 1:
+                new_node.next = current_node.next 
+                current_node.next = new_node
+                break
+
+            current_node = current_node.next
+            count += 1
+
+    def insert_after_value(self, target_value, data_to_insert):
+        if self.head is None:
+            return
+
+        if self.head.data == target_value:
+            new_node = Node(data_to_insert)
+            new_node.next = self.head.next
+            self.head.next = new_node
+            return
+
+        current_node = self.head
+        while current_node:
+            if current_node.data == target_value:
+                new_node = Node(data_to_insert)
+                new_node.next = current_node.next
+                current_node.next = new_node
+                break
+
+            current_node = current_node.next
+
+    def remove_by_value(self, value):
+        if self.head is None:
+            return
+
+        if self.head.data == value:
+            self.head = self.head.next
+            return
+
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.data == value:
+                    current_node.next = current_node.next.next
+                    break
+            current_node = current_node.next
+
+
+
+        
+
 
 list = LinkedList()
 
@@ -100,7 +190,14 @@ list.append(6)
 list.prepend(10)
 list.traverse()
 
-list_as_array = list.to_array()
-print(list_as_array)
+print(list.to_array())
 
-print(countNodes(list.head))
+print('---------')
+# list.remove_at(3)
+list.insert_at(3, 102)
+list.insert_after_value(102, 19012)
+print(list.to_array())
+list.insert_after_value(4, 'EU SOU FODA')
+list.remove_by_value(6)
+
+print(list.to_array())
